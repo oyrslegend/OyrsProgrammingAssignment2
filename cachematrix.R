cae0ddc2e355523cfd363b8618f3a7e54b28660b
oyrslegend <119655091+oyrslegend@users.noreply.github.com>
2022-12-10 17:06:23 +0700

    Oyrs

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##SHA1 
+##8012dea5d9712eb54b6559b6dbbab15ebc76e44b
 
 makeCacheMatrix <- function(x = matrix()) {
+     z <- NULL
+     set <- function(y) {
+         x <<- y
+         z <<- NULL ## function when its going to inverse
+     }
+     get <- function() x
+     setInverse <- function(inverse) z <<- inverse
+     getInverse <- function() z
+     list(set = set,
+          get = get,
+          setInverse = setInverse, ##setup the inverse
+          getInverse = getInverse) 
+ }
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+> cacheSolve <- function(x, ...) {
+     ## Return a matrix that is the inverse of 'x'
+     z <- x$getInverse()
+     if (!is.null(z)) {
+         message(" Inversed data") ##Condition when the matrix already get inversed
+         return(z)
+     }
+     matrix <- x$get() ##matrix print
+     z <- solve(mat, ...)
+     x$setInverse(z)
+    z
+ }
